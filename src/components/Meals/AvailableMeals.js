18f767299bb18850f300d50d06ba4b4c6fd4d65d
Jeton Ramadani <Jeton.Ramadani@intecsystem.com>
2021-11-09 16:28:36 +0100

    Added card and meal item component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { DUMMY_MEALS } from "./dummy-meals";
 import classes from "./AvailableMeals.module.scss";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
-      <ul>
-        {DUMMY_MEALS.map((meal) => (
-          <li key={meal.id}>{meal.name}</li>
-        ))}
-      </ul>
+      <Card>
+        <ul>
+          {DUMMY_MEALS.map((meal) => (
+            <MealItem key={meal.id} {...meal} />
+          ))}
+        </ul>
+      </Card>
     </section>
   );
 };
